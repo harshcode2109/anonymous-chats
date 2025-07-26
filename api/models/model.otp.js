0908990027016db0e2d2841e4bde1c8b7bcbb114
harshcode2109 <156992122+harshcode2109@users.noreply.github.com>
2025-07-27 00:43:38 +0530

    commit

---
+++ api/models/model.otp.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const otpSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    expires: 300,
+  },
+},{ timestamps: true });
+
+const OTP = mongoose.model("OTP", otpSchema);
+export default OTP;
